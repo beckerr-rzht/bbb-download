883f15ec2497a7be4ff544f2de1d1a49840232f1
Hamid <Hamid.Najafi@email.com>
2021-04-12 01:46:01 +0430

    bbb-2.3 support added

--- install.sh
+++ install.sh
@@ -12,7 +12,21 @@ fi
 apt-get install -y python
 
 # Install additional libs need to convertation process
-apt-get install -y curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0 libva-x11-1 libva-drm1 libfdk-aac-dev
+if [[ $(lsb_release -rs) == "18.04" ]]; then
+    apt-get install -y curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0 libfdk-aac-dev
+    # http://repos.interior.udelar.edu.uy/ubuntu/archives/pool/universe/libv/libva/
+    wget http://repos.interior.udelar.edu.uy/ubuntu/archives/pool/universe/libv/libva/libva1_1.8.3-2_amd64.deb
+    apt install ./libva1_1.8.3-2_amd64.deb 
+    wget http://repos.interior.udelar.edu.uy/ubuntu/archives/pool/universe/libv/libva/libva-x11-1_1.8.3-2_amd64.deb
+    apt install ./libva-x11-1_1.8.3-2_amd64.deb 
+    wget http://repos.interior.udelar.edu.uy/ubuntu/archives/pool/universe/libv/libva/libva-drm1_1.8.3-2_amd64.deb
+    apt install ./libva-drm1_1.8.3-2_amd64.deb
+if [[ $(lsb_release -rs) == "16.04" ]]; then 
+    apt-get install -y curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0 libva-x11-1 libva-drm1 libfdk-aac-dev
+else
+    echo "Non-compatible version"
+    exit 0
+fi
 
 # Copy ffmpeg to /opt/ffmpeg folder
 mkdir -p /opt/ffmpeg
@@ -20,15 +34,16 @@ cp ffmpeg/* /opt/ffmpeg
 chmod 755 /opt/ffmpeg/ffmpeg
 chmod 755 /usr/local/bigbluebutton/core/scripts/post_publish/*
 
-#Set up NGINX to make MP4 files available for createwebinar.com
+# Set up NGINX to make MP4 files available for createwebinar.com
 mkdir /var/www/bigbluebutton-default/download
 ln -s /var/bigbluebutton/published/presentation /var/www/bigbluebutton-default/download
 chmod 0755 /var/bigbluebutton/published/presentation
+# sudo chown -R bigbluebutton:bigbluebutton /var/bigbluebutton
 
 # Create log directory
 mkdir -p /var/log/bigbluebutton/download
-chown tomcat7:tomcat7 /var/log/bigbluebutton/download
-chmod -R go+rw /var/log/bigbluebutton/download/
+chown -R bigbluebutton:bigbluebutton /var/log/bigbluebutton/download
+chmod -R ugo+rw /var/log/bigbluebutton/download/
 
 # Copy python scripts to post_publish directory
 cp src/*.py /usr/local/bigbluebutton/core/scripts/post_publish
