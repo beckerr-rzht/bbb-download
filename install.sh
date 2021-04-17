b5d4b56fb7e9c4af00916acb6d25d6cec649d6a6
Hamid <Hamid.Najafi@email.com>
2021-04-18 03:43:19 +0430

    init

--- install.sh
+++ install.sh
@@ -58,4 +58,5 @@ echo "Coping python scripts to post_publish directory"
 cp $scriptDir/src/*.py /usr/local/bigbluebutton/core/scripts/post_publish
 echo "Set permisions"
 sudo chmod -R 755 /usr/local/bigbluebutton/core/scripts/post_publish/
-echo "Done."
+
+echo "BBB-Download Installaltion Done."
